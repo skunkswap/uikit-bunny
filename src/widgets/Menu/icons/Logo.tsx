a179abc48280d2947799292bd018aeb05d54ebf1
skunkswap <85455810+skunkswap@users.noreply.github.com>
2021-07-02 17:50:18 +0200

    Update Logo.tsx

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -10,7 +10,7 @@ const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
     <Svg viewBox="0 0 205 26" {...props}>
-      <image width="205" height="26" href={isDark ? '/images/skunk/LogoTextNewDark.png' : '/images/skunk/LogoTextNewWhite.png'}/>
+      <image width="205" height="26" href={isDark ? '/images/bunny/LogoTextNewDark.png' : '/images/bunny/LogoTextNewWhite.png'}/>
     </Svg>
   )
 };
